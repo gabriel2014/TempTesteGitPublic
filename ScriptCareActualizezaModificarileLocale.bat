ac8626863e01b40926e6f0a27a48f721551f0737
alexcutieru <alexcutieru.93@gmail.com>
2017-09-28 20:46:01 +0300

    actualizare date locale

--- ScriptCareActualizezaModificarileLocale.bat
+++ ScriptCareActualizezaModificarileLocale.bat
@@ -1,6 +1,7 @@
 rem Script-ul trebuie lansat din directorul proiectului git 
-SET PATH=%PATH%;C:\Program Files\Git\bin
+SET PATH=%PATH%;D:\Alex\Git\bin
 START /WAIT git add . 
 START /WAIT git commit -m "actualizare date locale"
 START /WAIT git push origin  
+START /WAIT git pull origin
 exit 
